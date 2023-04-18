6db352032fda8c834bf418a9b03ce671b0e2e288
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-04-18 20:41:56 +0800

    refactor the helloworld sections to be dynamic

---
+++ sections/policies.js
@@ -0,0 +1,9 @@
+const policiesLinks = {
+  plagiarism:
+    'https://www.uow.edu.au/student/support-services/learning-development/plagiarism/',
+  academicConsiderations:
+    'https://www.uow.edu.au/student/admin/academic-consideration/portal/',
+  studentSupport: 'https://www.uow.edu.au/student/support-services/advisers/',
+  disabilitiesSupport:
+    'https://www.uow.edu.au/student/support-services/disability/',
+}
