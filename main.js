bf8bba14ce70ef46232b4ef445231b86e3310314
Jayden <yawzyn001@mymail.sim.edu.sg>
2023-04-22 01:48:48 +0800

    refactor

--- main.js
+++ main.js
@@ -7,32 +7,83 @@ import './sections/helloworld'
 import './sections/policies'
 import './sections/form'
 
-const date = document.getElementById('date')
-const nameInput = document.getElementById('name')
-const moduleCodeInput = document.getElementById('module_code')
-const form = document.getElementById('form')
-const errorMessage = document.getElementById('error')
+const message = document.getElementById('message')
+const find = document.getElementById('find')
+const replace = document.getElementById('replace')
+const changeBtn = document.getElementById('change')
+const changeCount = document.getElementById('changeCount')
+const resetBtn = document.getElementById('reset-btn')
 
-date.valueAsDate = new Date()
+window.onload = showDate
 
-form.addEventListener('submit', (event) => {
-  let messages = []
-  const nameRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
-  const moduleCodeRegex = /^[A-Z][a-z][2-9]{3}$/
+function resetForm() {
+  document.getElementById('form').reset()
+  setTimeout(function () {
+    showDate()
+  }, 0)
+}
 
-  if (nameRegex.test(nameInput.value)) {
-    messages.push('Please do not use special characters in your name.')
+resetBtn.addEventListener('click', () => {
+  resetForm()
+})
+
+//disable / enable inputs
+find.addEventListener('keyup', () => {
+  if (find.value != '') {
+    replace.disabled = false
+    changeBtn.disabled = false
   }
-
-  if (
-    moduleCodeInput.value.length > 0 &&
-    !moduleCodeRegex.test(moduleCodeInput.value)
-  ) {
-    messages.push('Please enter a valid module code.')
-  }
-
-  if (messages.length > 0) {
-    event.preventDefault()
-    alert(messages.join('\n'))
+  if (find.value == '') {
+    replace.disabled = true
+    changeBtn.disabled = true
   }
 })
+
+//find and replace function
+changeBtn.addEventListener('click', () => {
+  let replacedText = message.value.replaceAll(find.value, replace.value)
+  let count = (message.value.match(new RegExp(find.value, 'g')) || []).length
+  message.value = replacedText
+  changeCount.textContent = count + ' replacements made.'
+})
+
+function showDate() {
+  // Get the current date
+  var now = new Date()
+
+  // Define arrays for month names and day names
+  var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+  // Format the date string
+  var dateString =
+    months[now.getMonth()] +
+    ' ' +
+    now.getDate() +
+    ', ' +
+    now.getFullYear() +
+    ' ' +
+    days[now.getDay()] +
+    ' ' +
+    now.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true,
+    })
+
+  // Set the textbox value to the formatted date
+  document.getElementById('Date').value = dateString
+}
--- sections/form.js
+++ sections/form.js
@@ -1,39 +1,68 @@
 const formSection = document.getElementById('form-section')
 
 formSection.innerHTML = `
-    <form id="form" action="https://translate.google.com" method="get">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" required> <br>
-        <label for="module_code">Module Code:</label>
-        <input type="text" name="module_code" id="module_code"> <br>
-        <label for="date">Date:</label>
-        <input type="date" name="date" id="date" disabled> <br>
-
-        <textarea rows="4" cols="10" name="text">Hello Test 202304</textarea>
-
-        <br>
-        Source language:
-        <br>
-
-        <input type="radio" id="English" name="sl" value="en" checked="checked">
-        <label for="English">English</label> <br>
-        <input type="radio" id="Malay" name="sl" value="ms">
-        <label for="Malay">Malay</label>
-
-        <br>
-        Target language:
-        <br>
-
-        <input type="radio" id="Malay" name="tl" value="ms" checked="checked">
-        <label for="Malay">Malay</label> <br>
-        <input type="radio" id="Chinese" name="tl" value="zh">
-        <label for="Chinese">Chinese</label> <br>
-        <input type="radio" id="Japanese" name="tl" value="ja">
-        <label for="Japanese">Japanese</label> <br>
-        <input type="radio" id="Korean" name="tl" value="ko">
-        <label for="Korean">Korean</label> <br>
-
-        <button type="submit" value="Translate">Translate</button>
-    </form>
-    <div id="error"></div>
+    <form action="https://translate.google.com" id="form" method="get" onreset="gg()">
+      <table>
+        <tr>
+          <td class="border-none">Name </td>
+          <td class="border-none"><input type="text" pattern="[A-Za-z0-9]+" name="username" id="username"
+              title="Must not contain special characters"></td>
+        </tr>
+        <tr>
+          <td class="border-none">Module Code</td>
+          <td class="border-none"><input type="text" pattern="^[A-Z][a-z][2-9]{3}$" name="module" id="username"></td>
+        </tr>
+        <tr>
+          <td class="border-none">Current Date</td>
+          <td class="border-none"><input type="" disabled id="Date"></td>
+        </tr>
+        <tr>
+          <td class="border-none">Message</td>
+          <td class="border-none"><textarea name="text" id="message" cols="10" rows="4">Hello Test 202304</textarea></td>
+        </tr>
+        <tr>
+          <td class="border-none">Find</td>
+          <td class="border-none"><input type="text" name="find" id="find"></td>
+        </tr>
+        <tr>
+          <td class="border-none">Replace</td>
+          <td class="border-none"><input type="text" name="replace" disabled id="replace"></td>
+        </tr>
+        <tr>
+          <td class="border-none">
+            <button id="change" type="button" disabled>Find and Replace</button>
+          <td class="border-none" id="changeCount"></td>
+          </td>
+        </tr>
+        <tr>
+          <td class="border-none">
+            <p>Source Language</p>
+            <input id="ENG" type="radio" name="sl" value="en" checked>
+            <label for="ENG">English</label>
+            <br>
+            <input id="MY" type="radio" name="sl" value="ms">
+            <label for="MY">Malay</label>
+          </td>
+        </tr>
+        <tr>
+          <td class="border-none">
+            <label for="targetlanguage">Target Language</label>
+            <select id="targetlanguage" name="tl">
+              <option value="zh-CN">Chinese</option>
+              <option selected value="ms">Malay</option>
+              <option value="ja">Japanese</option>
+              <option value="ko">Korean</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td class="border-none"><input type="submit" value="Translate"></td>
+        </tr>
+        <tr>
+          <td class="border-none">
+            <input type="button" id="reset-btn" value="reset">
+          </td>
+        </tr>
+      </table>
+  </form>
 `
